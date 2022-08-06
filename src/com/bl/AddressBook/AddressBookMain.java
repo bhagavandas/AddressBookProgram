79fad2750c939ae51865d0dc0578aaf9e1d096d7
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-06 08:14:40 +0530

    Added: Welcome to Address Book

---
+++ src/com/bl/AddressBook/AddressBookMain.java
@@ -0,0 +1,19 @@
+package com.bl.AddressBook;
+
+import java.util.ArrayList;
+
+public class AddressBookMain {
+	ArrayList<Contact> addressbook = new ArrayList<>();
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book Program");
+		Contact contact = new Contact();
+		//contact.addNewContact();
+		//contact.createContacts();
+		//contact.editContact();
+		
+		
+		
+	}
+}
