e0f8503a0075e92e2b4bfcbabe56b0f56d2a3712
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-27 11:22:55 +0530

    Added: Sorting the entries in addressbook by user details

--- src/com/bl/AddressBook/PersonDetails.java
+++ src/com/bl/AddressBook/PersonDetails.java
@@ -9,51 +9,78 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class PersonDetails {
-		
+
 	// sorting the person details in addressbook using stream functions of sorted
 	public static void sortPersonDetails() {
 		Contact contact = new Contact();
-		
-		/*created an arraylist 
-		 * added user details
-		 * it adds the read method in Contact class
+
+		/*
+		 * created an arraylist added user details it adds the read method in Contact
+		 * class
 		 */
 		List<Contact> contact1 = new ArrayList<Contact>();
-		
+
 		contact1.add(contact.read());
-		
-		/*created an arraylist 
-		 * added user details
-		 * it adds the read method in Contact class and stored in contacts array list
+
+		/*
+		 * created an arraylist added user details it adds the read method in Contact
+		 * class and stored in contacts array list
 		 */
-		 ArrayList<Contact> contacts2 = new ArrayList<>(Arrays.asList());
-		 contacts2.add(contact.read());
+		ArrayList<Contact> contacts2 = new ArrayList<>(Arrays.asList());
+		contacts2.add(contact.read());
 
 		// created an arraylist to add multiple contacts in it
-		 ArrayList<Contact>  contacts = 
-	                new ArrayList<Contact> ();
-			
-			contacts.addAll(contact1);
-			contacts.addAll(contacts2);
+		ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+		contacts.addAll(contact1);
+		contacts.addAll(contacts2);
 
 //
 //		List<Contact> sortedList = contacts.stream().sorted().collect(Collectors.toList());
 //		 
-//				System.out.println("Sorted List: " + sortedList.toString());
+
+		// sorting the entries in addressbook the address book by name
+		System.out.println("Sorted by person name: ");
 //		
-		        List<Contact> sortedList = contacts.stream()
-					.sorted(Comparator.comparing(Contact::getFirstName))
-					.collect(Collectors.toList());
-		 
-		        sortedList.forEach(System.out::println);
-				
+		List<Contact> sortedList = contacts.stream().sorted(Comparator.comparing(Contact::getFirstName))
+				.collect(Collectors.toList());
+
+		sortedList.forEach(System.out::println);
+		System.out.println(".....................");
+
+		// sorting the entries in addressbook the address book by City
+		System.out.println("Sorted by city: ");
+
+		List<Contact> sortedList1 = contacts.stream().sorted(Comparator.comparing(Contact::getCity))
+				.collect(Collectors.toList());
+
+		sortedList.forEach(System.out::println);
+		System.out.println(".....................");
+
+		// sorting the entries in addressbook the address book by State
+		System.out.println("Sorting by state: ");
+//						
+		List<Contact> sortedList2 = contacts.stream().sorted(Comparator.comparing(Contact::getState))
+				.collect(Collectors.toList());
+
+		sortedList.forEach(System.out::println);
+		System.out.println(".....................");
+
+		// sorting the entries in addressbook the address book by zip
+		System.out.println("Sorting by zip: ");
+//								
+		List<Contact> sortedList3 = contacts.stream().sorted(Comparator.comparing(Contact::getZip))
+				.collect(Collectors.toList());
+
+		sortedList.forEach(System.out::println);
+		System.out.println(".....................");
+
 	}
-	
+
 	// UC-11 using stream function
 	public static void main(String[] args) {
-		//calling the method
+		// calling the method
 		sortPersonDetails();
 	}
-	
-	
+
 }
